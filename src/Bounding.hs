59c6b82e63c9d4d094171dc99c0612b7e91a6a05
bhaaksema <17254355+bhaaksema@users.noreply.github.com>
2024-01-19 16:54:13 +0000

    Bouding function documentation

--- src/Bounding.hs
+++ src/Bounding.hs
@@ -18,17 +18,22 @@ apply s (a :> b) = apply s a :> apply s b
 apply s (V str)  = s M.! str
 apply _ a        = a
 
+-- | All possible substitutions of an axiom
 subst :: Axiom -> S.Set Formula -> [Subst]
 subst a fs = map M.fromList $ sequence [[(v, f1) | f1 <- S.toList fs] | v <- S.toList (vars a)]
 
+-- | All possible instantiations of an axiom
 insts :: (t -> S.Set Formula) -> [Axiom] -> t -> S.Set Formula
 insts f axi e = S.fromList $ concat [map (`apply` a) (subst a (f e)) | a <- axi]
 
+-- | Variable-bounding function
 var :: [Axiom] -> Formula -> S.Set Formula
 var = insts (S.map V . vars)
 
+-- | Formula-bounding function
 for :: [Axiom] -> Formula -> S.Set Formula
 for = insts fors
 
+-- | Set-bounding function
 set :: [Axiom] -> Formula -> S.Set Formula
 set = insts cons
