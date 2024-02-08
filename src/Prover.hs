39e876b64a4ec2fdd0b2864b257e59bb10b9ba08
bhaaksema <17254355+bhaaksema@users.noreply.github.com>
2024-02-08 19:22:20 +0100

    Simpler data

--- src/Prover.hs
+++ src/Prover.hs
@@ -14,15 +14,15 @@ sprove ax = iprove . embed ax
 
 -- | Prove a intuitionistic theorem (m-G4ip)
 iprove :: Formula -> Bool
-iprove = prove Int . singleton . F . simply
+iprove = prove Int . (<+ empty) . F . simply
 
 -- | Prove a classical theorem (G3cp)
 cprove :: Formula -> Bool
-cprove = prove Cl . singleton . F . simply
+cprove = prove Cl . (<+ empty) . F . simply
 
 -- | Schedule formulae based on proof rules
-schedule :: Sign Formula -> Prio
-schedule a = case a of
+(<+) :: Sign Formula -> Sequent -> Sequent
+(<+) a | i <- case a of
   T (Var _)        -> P1
   F (Var _)        -> P1
   T (Var _ :> Bot) -> P1
@@ -31,40 +31,39 @@ schedule a = case a of
   T (_ :| _)       -> P3
   T (_ :> _)       -> P4
   _                -> P0
+  = ((i, a) <|)
+infixr 4 <+
 
 -- | Check provability depending on the logic
 prove :: Logic -> Sequent -> Bool
 prove l y = case view y of
-  -- Search exhausted
-  Empty -> False
-  -- Schedule fresh (+>) formulae
-  Fresh (a, x) -> prove l ((schedule a, a) |> x)
-  -- Resolve scheduled (|>) formulae
-  Stale (e, x) -> case e of
+  Just (e, x) -> case e of
     -- Initial sequents
     T Bot -> True; F Top -> True
     -- Unary premise rules
     T Top -> prove l x; F Bot -> prove l x
-    T (a :& b) -> prove l (T a +> T b +> x)
-    F (a :| b) -> prove l (F a +> F b +> x)
-    T ((c :& d) :> b) -> prove l (T (c :> d :> b) +> x)
-    T ((c :| d) :> b) -> prove l (T (c :> b) +> T (d :> b) +> x)
+    T (a :& b) -> prove l (T a <+ T b <+ x)
+    F (a :| b) -> prove l (F a <+ F b <+ x)
+    T ((c :& d) :> b) -> prove l (T (c :> d :> b) <+ x)
+    T ((c :| d) :> b) -> prove l (T (c :> b) <+ T (d :> b) <+ x)
     -- Replacement rules
-    (T (Var p)) -> prove l (mapSubsti True p Top x)
-    (F (Var p)) -> prove l ((P5, e) |> mapSubsti False p Bot x)
-    (T (Var p :> Bot)) -> prove l (mapSubsti True p Bot x)
+    (T (Var p)) -> prove l (mapSubsti (<+) True p Top x)
+    (F (Var p)) -> prove l ((P5, e) <| mapSubsti (<+) False p Bot x)
+    (T (Var p :> Bot)) -> prove l (mapSubsti (<+) True p Bot x)
     -- Right implication
     (F (a :> b))
-      | l == Cl || nullFs x -> prove l (T a +> F b +> x)
-      | prove l (T a +> F b +> delFs x) -> True
+      | l == Cl || nullFs x -> prove l (T a <+ F b <+ x)
+      | prove l (T a <+ F b <+ delFs x) -> True
     -- Right conjunction
-    (F (a :& b)) -> all (prove l) [F a +> x, F b +> x]
+    (F (a :& b)) -> all (prove l) [F a <+ x, F b <+ x]
     -- Left disjunction
-    (T (a :| b)) -> all (prove l) [T a +> x, T b +> F a +> x]
+    (T (a :| b)) -> all (prove l) [T a <+ x, T b <+ F a <+ x]
     -- Left implication
     (T (a :> b))
-      | l == Cl || nullFs x -> all (prove Cl) [T b +> x, F a +> F b +> x]
-      | (c :> d) <- a -> all (any (prove l)) [[T b +> x],
-        [T c +> T (d :> b) +> F d +> delFs x, F a +> F b +> (P5, e) |> x]]
+      | l == Cl || nullFs x -> all (prove Cl) [T b <+ x, F a <+ F b <+ x]
+      | (c :> d) <- a -> all (any (prove l)) [[T b <+ x],
+        [T c <+ T (d :> b) <+ F d <+ delFs x, F a <+ F b <+ (P5, e) <| x]]
     -- Backtracking
-    a -> prove l ((P5, a) |> x)
+    a -> prove l ((P5, a) <| x)
+  -- Search exhausted
+  Nothing -> False
--- src/Sequent.hs
+++ src/Sequent.hs
@@ -13,45 +13,36 @@ data Sign a = T a | F a
 data Prio = P0 | P1 | P2 | P3 | P4 | P5
   deriving (Eq, Ord, Show, Enum, Bounded)
 
--- | View for sequent
-data View a = Fresh a | Stale a | Empty
-
 -- | Sequent is a set of prioritised signed formulae
-type SFormula = Sign Formula
-type Sequent = ([SFormula], S.Set (Prio, SFormula))
+type Sequent = S.Set (Prio, Sign Formula)
 
--- | \(O(\log n)\). Sequent with one signed formula
-singleton :: SFormula -> Sequent
-singleton a = ([a], S.empty)
+-- | \(O(1)\). Empty sequent
+empty :: Sequent
+empty = S.empty
+
+-- | \(O(\log n)\). Insert a signed formula
+(<|) :: (Prio, Sign Formula) -> Sequent -> Sequent
+(<|) = S.insert
+infixr 4 <|
 
 -- | \(O(\log n)\). Retrieve the signed formula with first priority
-view :: Sequent -> View (SFormula, Sequent)
-view (a : as, x) = Fresh (a, (as, x))
-view (_, x) | Just ((i, a), y) <- S.minView x, i < maxBound = Stale (a, ([], y))
-view _ = Empty
-
--- | \(O(\log n)\). Insert a fresh signed formula
-(+>) :: SFormula -> Sequent -> Sequent
-a +> (as, x) = (a : as, x)
-infixr 4 +>
-
--- | \(O(\log n)\). Insert a scheduled formula
-(|>) :: (Prio, SFormula) -> Sequent -> Sequent
-a |> (as, x) = (as, S.insert a x)
-infixr 4 |>
+view :: Sequent -> Maybe (Sign Formula, Sequent)
+view x | Just ((i, a), y) <- S.minView x, i < maxBound = Just (a, y)
+view _ = Nothing
 
 -- | \(O(n)\). Check if the sequent contains no F-signed formulae
 nullFs :: Sequent -> Bool
-nullFs (_, x) = S.null $ S.filter isF x
+nullFs x = S.null $ S.filter isF x
   where isF (_, F _) = True; isF _ = False
 
 -- | \(O(n)\). Remove all F-signed formulae
 delFs :: Sequent -> Sequent
-delFs (as, x) = (as, S.filter isT x)
+delFs = S.filter isT
   where isT (_, T _) = True; isT _ = False
 
 -- | \(O(n \log n)\). Substitute sequent, can reset priority
-mapSubsti :: Bool -> String -> Formula -> Sequent -> Sequent
-mapSubsti t p c (as, x) = S.foldr (\(i, a) -> let b = f a in
-  (if a /= b then (b +>) else ((i, b) |>))) (map f as, S.empty) x
+mapSubsti :: (Sign Formula -> Sequent -> Sequent)
+  -> Bool -> String -> Formula -> Sequent -> Sequent
+mapSubsti pinsert t p c = S.foldr (\(i, a) -> let b = f a in
+  if a /= b then pinsert b else ((i, b) <|)) S.empty
   where f = (unitSubsti t (p, c) <$>)
