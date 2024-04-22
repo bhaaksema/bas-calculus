068be53a997eb7fe09e91c87bb94a34b66554fb9
bhaaksema <17254355+bhaaksema@users.noreply.github.com>
2024-04-22 18:13:58 +0200

    Omega-propagation

--- src/Prover/Super.hs
+++ src/Prover/Super.hs
@@ -6,21 +6,9 @@ import qualified Data.Set    as S
 import           Data.Formula
 import qualified Prover.Intuition as Int
 
--- | Axiom is kind of a formula
-type Axiom = Formula
-
--- | Prove a superintuitionistic theorem
-prove :: [Axiom] -> Formula -> Bool
-prove ax = Int.prove . embed ax
-
--- | Embed an intermediate logic into intuitionistic logic
-embed :: [Axiom] -> Formula -> Formula
-embed axi f = foldr (:&) Top (bfunc axi $ cons f) :> f
-
--- | Generalized bounding function
-bfunc :: [Axiom] -> S.Set Formula -> [Formula]
-bfunc axi as = [fullSubsti m ax | ax <- axi,
-  m <- foldr (\p -> (M.insert p <$> S.toList as <*>)) [M.empty] (vars ax)]
+-- | Axiomatization kinds
+data Axiomatization = VAR | FOR | SET
+  deriving (Eq, Ord, Show)
 
 -- | Set of variables of a formula
 vars :: Formula -> S.Set Int
@@ -40,3 +28,32 @@ fors a        = S.singleton a
 -- | Set of conjunctions of subformulas of a formula
 cons :: Formula -> S.Set Formula
 cons = S.map (foldr1 (:&)) . S.deleteMin . S.powerSet . fors
+
+-- | Convert Axiomatization to corresponding formula
+toFunction :: Axiomatization -> (Formula -> S.Set Formula)
+toFunction VAR = S.map Var . vars
+toFunction FOR = fors
+toFunction SET = cons
+
+-- Upwards propagation
+upwards :: (Formula -> Formula -> Formula) -> Formula -> Formula -> Int -> Bool
+upwards op ax q p | r <- succ q = Int.prove (subap q :& subap r :> subap (op q r))
+  where subap b = unitSubsti True (p, b) ax
+
+-- Check the kind of axiomatization
+axiomatization :: Formula -> Axiomatization
+axiomatization ax | allUp (:&)
+  = if allUp (:|) && allUp (:>) then VAR else FOR
+  where allUp op = all (upwards op ax (succ ax)) (vars ax)
+axiomatization _ = SET
+
+-- | Generalized bounding function
+bfunc :: [Formula] -> Formula -> [Formula]
+bfunc axioms f = let
+  fs = (toFunction $ maximum $ map axiomatization axioms) f
+  in [fullSubsti m ax | ax <- axioms,
+  m <- foldr (\p -> (M.insert p <$> S.toList fs <*>)) [M.empty] (vars ax)]
+
+-- | Prove a superintuitionistic theorem via intuitionistic logic embedding
+prove :: [Formula] -> Formula -> Bool
+prove axioms f = Int.prove (foldr (:&) Top (bfunc axioms f) :> f)
